3f78f080bada5d53123f0c0218a7e08cb0a2cdc2
micosara <micosara365@gamil.com>
2025-09-02 11:48:31 +0900

    Update index.jsx

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
+import { FlatList } from "react-native";
 import {
     Text, View
 } from "react-native-web";
@@ -38,6 +39,11 @@ export default function MovieListScreen() {
     return (
         <View>
             <Text>무비리스트</Text>
+            <FlatList 
+                data={data.results}
+                renderItem={()=>{}}
+                keyExtractor={(item) => item.id.toString()}
+            />
         </View>
     )
 }
